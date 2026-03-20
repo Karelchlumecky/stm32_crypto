4f157de65ea9a9dc78e074de9780cd12cda1be5a
Karel Chlumecky <karelchlumecky552@seznam.cz>
2026-03-20 20:20:02 +0100

    Support for print all hashes and function from get hash function.

--- source/main.py
+++ source/main.py
@@ -6,7 +6,7 @@ For STM32.
 import argparse
 from intelhex import IntelHex
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import serialization, hashes
 
 class VaribleBank:
     Sign_address = 0
@@ -43,6 +43,9 @@ class VaribleBank:
             (cls.Private_key_file_pem,
              cls.Public_key_file_pem) = args.generate
 
+        elif args.print:
+            cls.Action = "Print"
+
     @classmethod
     def print_args(cls):
         print(f"""
@@ -51,9 +54,43 @@ class VaribleBank:
               Sign Address: {cls.Sign_address}
               Start Address: {cls.S_address}
               End Address: {cls.E_address}
-              Hash: {cls.Hash} 
+              Hash: {cls.Hash}
               Private key:{cls.Private_key_file_pem}
               Public key: {cls.Public_key_file_pem}""")
+
+def print_supported_hashes():
+    print("""
+        SHA224, SHA256, SHA384, SHA512,
+        SHA3-224, SHA3-256, SHA3-384, SHA3-512,
+        SHAKE128_X -> X = Number of bytes
+        SHAKE256_X -> X = Number of bytes
+    """)
+
+def choose_hash():
+    if VaribleBank.Hash == "SHA224":
+        return hashes.SHA224()
+    elif VaribleBank.Hash == "SHA256":
+        return hashes.SHA256()
+    elif VaribleBank.Hash == "SHA384":
+        return hashes.SHA384()
+    elif VaribleBank.Hash == "SHA512":
+        return hashes.SHA512()
+    elif VaribleBank.Hash == "SHA3-224":
+        return hashes.SHA3_224()
+    elif VaribleBank.Hash == "SHA3-256":
+        return hashes.SHA3_256()
+    elif VaribleBank.Hash == "SHA3-384":
+        return hashes.SHA3_384()
+    elif VaribleBank.Hash == "SHA3-512":
+        return hashes.SHA3_512()
+    elif "SHAKE128" in VaribleBank.Hash:
+        _, size = VaribleBank.Hash.split("_")
+        return hashes.SHAKE128(size)
+    elif "SHAKE256" in VaribleBank.Hash:
+        _, size = VaribleBank.Hash.split("_")
+        return hashes.SHAKE256(size)
+    else:
+        print("Wrong hash choosed")
 
 def verify():
     print("nvm")
@@ -87,22 +124,25 @@ def generate():
             )
         )
 
-
-
 def parse_input_params():
 
     parser = argparse.ArgumentParser("Cyber security program")
 
     pars_group = parser.add_mutually_exclusive_group(required=True)
     pars_group.add_argument("-g", "--generate", nargs = 2,
-                            metavar = ("Private_key_file", "Public_key_file"))
+                            metavar = ("Private_key_file", "Public_key_file"),
+                            help = "Generate private and public key")
     pars_group.add_argument("-s", "--sign", nargs=6,
                         metavar = ("Hex_file", "Sign_address", "Start_address",
                                    "End_address", "Hash", "Private_key_file"),
                         help="Sign your program")
     pars_group.add_argument("-v", "--verify", nargs=6,
                         metavar = ("Hex_file", "Sign_address", "Start_address",
-                                    "End_address", "Hash", "Public_key_file"))
+                                    "End_address", "Hash", "Public_key_file"),
+                        help="Verify your program")
+    pars_group.add_argument("-p", "--print", action="store_true",
+                            help = "Print all supported Hashes")
+
     args = parser.parse_args()
     VaribleBank.load_args(args)
 
@@ -118,6 +158,9 @@ def main():
 
     elif VaribleBank.Action == "Generate":
         generate()
+
+    elif VaribleBank.Action == "Print":
+        print_supported_hashes()
 
     else:
         print("Unrecognized action")
